2436bdc2df346e431d40f4ecd84cfcffc3ea96b7
LeeChangHoon <changhoon1030@gmail.com>
2022-12-25 00:12:53 +0900

    ⭐ feat : GlobalStyle

---
+++ styles/GlobalStyles.tsx
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from './css';
+
+export const GlobalStyles = createGlobalStyle`
+  ${reset}
+  * {
+  }
+  :root{
+    --color-black : #0E0301;
+    --color-white :#ffffff;
+    --color-lightPink : #ffc2c3;
+    --color-coral : #fe7773;
+    --color-rose : #e91E25 
+
+  }
+  html {
+    font-size: 62.5%;
+    box-sizing: border-box;
+    color: var(--color-black);
+    background-color:var(--color-coral);
+  }
+  body {
+    font-family: 'Noto Serif KR', serif;
+  }
+`;

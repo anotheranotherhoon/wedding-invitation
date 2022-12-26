3c79981551c5903ac17121308a85e23d118fe5c8
LeeChangHoon <changhoon1030@gmail.com>
2022-12-27 01:25:04 +0900

    ⭐ feat : 정적인 json파일 불러오기

---
+++ pages/api/staticdata.tsx
@@ -0,0 +1,8 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+
+export default async function handler(req : any, res : any) {
+  const jsonDirectory = path.join(process.cwd(), 'json');
+  const fileContents = await fs.readFile(jsonDirectory + '/invitationData.json', 'utf8');
+  res.status(200).json(fileContents);
+}

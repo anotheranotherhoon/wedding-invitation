b87dd68b44fb25ae4aa4fc8fa7cb83edd3c79f6f
LeeChangHoon <changhoon1030@gmail.com>
2022-12-28 15:22:00 +0900

    ⭐ feat : 모바일 클립보드 복사 테스트

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -27,8 +27,23 @@ const Modal = ({ bankState, accountNumberState, nameState, closeModal }: IModalP
         .then(() => {
           copyAccountNumber()
         })
-    } 
+    }else {
+      if (!document.queryCommandSupported("copy")) {
+        return alert("복사하기가 지원되지 않는 브라우저입니다.");
+      }
+      const textarea = document.createElement("textarea");
+      textarea.value = text;
+      textarea.style.position = "fixed";
+      document.body.appendChild(textarea);
+      textarea.focus();
+      textarea.select();
+      document.execCommand("copy");
+      document.body.removeChild(textarea);
+      copyAccountNumber()
+    }
+
   };
+
   return (
     <Container>
       <Background ref={modalRef} onClick={cllickBackground} />

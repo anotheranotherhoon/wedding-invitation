2403f13850a14e8a3035a20aaa7f4bc7b7cecc31
LeeChangHoon <changhoon1030@gmail.com>
2023-01-11 22:05:46 +0900

    👏 chore : 사용하지 않는 파일 삭제

--- pages/api/staticdata.tsx
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-import { promises as fs } from 'fs';
-
-export default async function handler(req : any, res : any) {
-  const jsonDirectory = path.join(process.cwd(), 'json');
-  const fileContents = await fs.readFile(jsonDirectory + '/invitationData.json', 'utf8');
-  res.status(200).json(fileContents);
-}

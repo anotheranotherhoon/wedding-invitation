b06b9a1d8991097aefa4ddef16b39abfecdcfe4d
LeeChangHoon <changhoon1030@gmail.com>
2023-01-11 20:08:36 +0900

    ⭐ feat : isoToTimeStamp 타입 추가

--- utils/convertDate.tsx
+++ utils/convertDate.tsx
@@ -1,4 +1,4 @@
-export const isoToTimeStamp = (time : any) => {
+export const isoToTimeStamp = (time : string) => {
   const date = new Date(time);
   let convertedDate = date.getFullYear()+'년 ' + (date.getMonth()+1) + '월 '+date.getDate()+'일 ';
   return convertedDate

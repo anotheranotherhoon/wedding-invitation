8d37d66bef4f27e9bb664006c731699c156818de
LeeChangHoon <changhoon1030@gmail.com>
2023-03-12 02:14:41 +0900

    ⚒ refactor : 필요 없는 코드 제거

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,7 +15,6 @@ const Layout = ({children} : IChildren) => {
 const Container = styled.div`
         display:flex;
         flex-direction:column;
-        justify-content:center;
         align-items:center;
         `
 

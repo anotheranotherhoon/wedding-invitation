94225a41eab4135b66215ef02fb146dcf3442c2d
LeeChangHoon <changhoon1030@gmail.com>
2023-03-19 03:11:34 +0900

    👏 chore : interface 변경

--- src/hook/useModal.tsx
+++ src/hook/useModal.tsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
+
+interface IShowModal {
+  (bank : string, accountNumber : string, name : string) : void
+}
 export const useModal = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [bankState, setBankState] = useState<string>('')
   const [accountNumberState, setAccountNumberState] = useState<string>('')
   const [nameState, setNameState] = useState<string>('')
-  
-  interface IShowModal {
-    (bank : string, accountNumber : string, name : string) : void
-  }
-
   const showModal : IShowModal = (bank, accountNumber, name) => {
     setBankState(bank)
     setAccountNumberState(accountNumber)

f9fdaddefeb584433d5e2d0e1bb9c074a074c9d7
LeeChangHoon <changhoon1030@gmail.com>
2023-03-19 03:18:02 +0900

    🧱 build : next.config.js 설정 변경

--- next.config.js
+++ next.config.js
@@ -2,13 +2,28 @@
 const nextConfig = {
   reactStrictMode: true,
 }
+module.exports = {
+  async rewrites() {
+    return [
+      // `/kr` 경로를 처리하는 설정을 추가합니다.
+      {
+        source: '/kr/:path*',
+        destination: '/:path*',
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig
 
 module.exports = {
   i18n: {
-    locales: ['kr'],
+    locales: ['en', 'ko', 'kr'],
     defaultLocale: 'kr',
+  },
+  localeSubpaths: {
+    en: 'en',
+    ko: 'kr',
   },
   images: {
     domains: ['m.bomtvcard.com', 'drive.google.com'],

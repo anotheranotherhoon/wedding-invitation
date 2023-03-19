ce4be0e3e6f69a23eb144e9593e096700021e79b
LeeChangHoon <changhoon1030@gmail.com>
2023-03-19 12:40:50 +0900

    🎺 test : prefetch 테스트

--- next.config.js
+++ next.config.js
@@ -2,28 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-module.exports = {
-  async rewrites() {
-    return [
-      // `/kr` 경로를 처리하는 설정을 추가합니다.
-      {
-        source: '/kr/:path*',
-        destination: '/:path*',
-      },
-    ];
-  },
-};
 
 module.exports = nextConfig
 
 module.exports = {
   i18n: {
-    locales: ['en', 'ko', 'kr'],
+    locales: ['kr'],
     defaultLocale: 'kr',
-  },
-  localeSubpaths: {
-    en: 'en',
-    ko: 'kr',
   },
   images: {
     domains: ['m.bomtvcard.com', 'drive.google.com'],

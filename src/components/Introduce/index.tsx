74a109302c8b047156aee891eabb6ab525eec760
LeeChangHoon <changhoon1030@gmail.com>
2023-03-22 14:10:34 +0900

    👏 chore : 데이터 기입 정보 수정

--- src/components/Introduce/index.tsx
+++ src/components/Introduce/index.tsx
@@ -23,7 +23,7 @@ const Introduce = ({ date, time, place, hall_name, thumbNail }: IInfoProps) => {
             <div>어제의 너와 내가</div>
             <div>오늘의 우리가 되어</div>
             <div>서로 닮아가고 있는</div>
-            <div>현기와 향정, 결혼합니다.</div>
+            <div>신랑과 신부가 결혼합니다.</div>
             <br />
             <div>서로의 사랑을 약속하는 자리에</div>
             <div>소중한 여러분들을</div>
